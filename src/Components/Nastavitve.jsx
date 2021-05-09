5a867f9774a992825f80d46897f90eba3d1385c3
kristjanb <kristjan.bleiweis@lucis.si>
2021-05-09 10:36:27 +0200

    testing

--- src/Components/Nastavitve.jsx
+++ src/Components/Nastavitve.jsx
@@ -41,7 +41,7 @@ function Nastavitve(props) {
                 </button>
             </div>
 
-            <textarea rows="4" cols="50" style={{border: "1px solid black"}}>
+            <textarea rows="8" cols="50" style={{border: "1px solid black"}}>
               {sharp}
             </textarea>
         </div>
