448c6ab24c6e3730d1570bd03a471eeb668f04d9
kristjanb <kristjan.bleiweis@lucis.si>
2021-05-09 10:42:36 +0200

    added another texta rea

--- src/Components/Nastavitve.jsx
+++ src/Components/Nastavitve.jsx
@@ -5,46 +5,108 @@ import axios from 'axios';
 
 function Nastavitve(props) {
 
-    const [idSorta, setIdSorta] = useState('');
+    const [dataForBC, setDataforBc] = useState('');
+    const [dataFromBC, setDatafromBC] = useState('');
     const [sharp, setSharp] = useState('');
+    const [sharp1, setSharp1] = useState('');
 
-    const handleInputChange = (event) => {
+    const handleInputForDataToBlockchain = (event) => {
 
         console.log(event.target.value);
-        setIdSorta(event.target.value)
+        setDataforBc(event.target.value)
     }
 
-    const onSubmit = () => {
+
+    const handleInputForSharp = (event) => {
+
+        console.log(event.target.value);
+        setSharp1(event.target.value)
+    }
+
+    const onSubmitToSendToBC = () => {
         axios.post('http://localhost:3005/bitcoin', {
-                idSorta
+                dataForBC: dataForBC
             }).then(response => {
                 console.log("response", response);
                 setSharp(response.data.message);
                 
             });
     }
+
+    const toGetDatafromBC = () => {
+        axios.post('http://localhost:3005/bitcoin', {
+                sharp1: sharp1
+            }).then(response => {
+                console.log("response", response);
+                setDatafromBC(response.data.message);
+                
+            });
+    }
     return (
-        <div style={{padding: "20px"}}>
-            <div>
-                <label>Vnesi kar želiš zapisati v blockchain</label>
-                <br></br>
-                <br></br>
-                <input type="text" onChange={(event) => handleInputChange(event)}></input>
-            </div>
-                <br></br>
-            <div onClick={() => onSubmit()}>
-                <button
-                    className="action-button" 
-                    style={{height: "50px"}}   
-                >
-                    Send to blockchain
-                </button>
+
+        <div style={{display: "flex"}}>
+            <div style={{padding: "20px"}}>
+                <div>
+                    <label>Vnesi kar želiš zapisati v blockchain</label>
+                    <br></br>
+                    <br></br>
+                    <input type="text" onChange={(event) => handleInputForDataToBlockchain(event)}></input>
+                </div>
+                    <br></br>
+                <div onClick={() => onSubmitToSendToBC()}>
+                    <button
+                        className="action-button" 
+                        style={{height: "50px"}}   
+                    >
+                        Send to blockchain
+                    </button>
+                </div>
+
+                <textarea rows="15" cols="50" style={{border: "1px solid black"}}>
+                {sharp}
+                </textarea>
             </div>
 
-            <textarea rows="15" cols="50" style={{border: "1px solid black"}}>
-              {sharp}
-            </textarea>
+
+
+            <div style={{padding: "20px"}}>
+                <div>
+                    <label>Vnesi hash, da pogledamo v blockchain</label>
+                    <br></br>
+                    <br></br>
+                    <input type="text" onChange={(event) => handleInputForSharp(event)}></input>
+                </div>
+                    <br></br>
+                <div onClick={() => toGetDatafromBC()}>
+                    <button
+                        className="action-button" 
+                        style={{height: "50px"}}   
+                    >
+                        Get from blockchain
+                    </button>
+                </div>
+
+                <textarea rows="15" cols="50" style={{border: "1px solid black"}}>
+                    {dataFromBC}
+                </textarea>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
+        
     )
 }
 
